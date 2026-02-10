36d1a6fb548f19131518a537bc5efd6eef16a380
yaosuhuy <levanquanghuy8@gmail.com>
2026-02-10 08:10:05 +0700

    doing signup bloc

--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,1 +1,29 @@
-class UserSignUp {}
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/src/either.dart';
+
+class UserSignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  const UserSignUp(this.authRepository);
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signInWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpParams({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
